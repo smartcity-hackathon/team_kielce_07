1728da7101fbd120dbaa6fdeacbad9bb40739984
Wiktoria <wiktoria.tomzik@gmail.com>
2018-06-15 20:39:21 +0200

    Displaying offers

--- dodawarka/views.py
+++ dodawarka/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Offer
 
 
 def index(request):
-    return HttpResponse("Hejka. Jesteś w viewsach.")
+    offers = Offer.objects.all()
+    template = loader.get_template('dodawarka/index.html')
+    context = {
+        'offers': offers,
+    }
+    return HttpResponse(template.render(context, request))
