fcf9328f537fc989d3b51afdadbdc900351b27a7
Wiktoria <wiktoria.tomzik@gmail.com>
2018-06-15 18:25:56 +0200

    Removing idle apps

--- inplacement/settings.py
+++ inplacement/settings.py
@@ -32,8 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'dodawarka',
-    'porownywarka',
-    'wyszukiwarka',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
