9dd9593b080b1275f48755f1f36d08b78ab76bbb
Patryk <piotrowski@patryk.org>
2018-06-15 19:45:53 +0200

    settings.py

--- inplacement/settings.py
+++ inplacement/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django_extensions',
 ]
 
+google_api_key = 'AIzaSyDa7Na4ZJg4C_b2p4NtW9Ua7OvaiTrQ9bA'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
