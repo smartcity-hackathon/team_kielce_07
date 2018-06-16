df96fea25b54417b54506b3a37c5c55f8f57ba41
Patryk <piotrowski@patryk.org>
2018-06-16 04:34:42 +0200

    do porównywarki pole na offer

--- dodawarka/views.py
+++ dodawarka/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 
@@ -11,3 +14,17 @@ def index(request):
         'offers': offers,
     }
     return HttpResponse(template.render(context, request))
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
